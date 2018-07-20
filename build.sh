7bf4e03aea9e4b5ec0b8d35dcdd2300c5716832c
marco <marco@webguerilla.net>
2018-07-21 00:55:46 +0200

    new release

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ rm build/*
 riot --ext tag . build
 
 cat libs/*.js > release/linkchecker-latest.js
+cat *.js >> release/linkchecker-latest.js
 cat build/*.js >> release/linkchecker-latest.js
 
 closure-compiler libs/*.js > release/linkchecker-latest.min.js
+closure-compiler *.js >> release/linkchecker-latest.min.js
 closure-compiler build/*.js >> release/linkchecker-latest.min.js
 
 # TODO try:
