c945b38b36ce8a6137ee62a1a6a5f046f4cbc76e
marco <marco@webguerilla.net>
2018-12-20 20:20:37 +0100

    fixed csv download bug in firefox

--- functions.js
+++ functions.js
@@ -1,5 +1,27 @@
 'use strict';
 
+// MouseEvent polyfill for IE and Edge(?)
+(function (window) {
+	try {
+		new MouseEvent('test');
+		return false; // No need to polyfill
+	} catch (e) {
+		// Need to polyfill - fall through
+	}
+
+	// Polyfills DOM4 MouseEvent
+	var MouseEvent = function (eventType, params) {
+		params = params || { bubbles: false, cancelable: false };
+		var mouseEvent = document.createEvent('MouseEvent');
+		mouseEvent.initMouseEvent(eventType, params.bubbles, params.cancelable, window, 0, params.screenX || 0, params.screenY || 0, params.clientX || 0, params.clientY || 0, false, false, false, false, 0, null);
+
+		return mouseEvent;
+	}
+
+	MouseEvent.prototype = Event.prototype;
+	window.MouseEvent = MouseEvent;
+})(window);
+
 function loadFile(resourceURL, token, filename, callback) {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', resourceURL);
@@ -30,7 +52,9 @@ function loadFileDownloadCallback(filename, blobURL, blob, xhr, data) {
 		var a = document.createElement('a');
 		a.href = blobURL;
 		a.download = filename;
-		a.click();
+
+		//a.click(); // click does not work in Firefox if link is not in DOM
+		a.dispatchEvent(new MouseEvent('click')); // MouseEvent requires polyfill
 	}
 
 	(window.URL || window.webkitURL).revokeObjectURL(blobURL);
